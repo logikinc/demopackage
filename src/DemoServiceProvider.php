096f329c69f5b4a3a319b890050106acd7727a85
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-05-13 21:03:27 +0300

    Package: config setup and publish

--- src/DemoServiceProvider.php
+++ src/DemoServiceProvider.php
@@ -11,12 +11,24 @@ class DemoServiceProvider extends ServiceProvider
 		$this->loadRoutesFrom(__DIR__ . '/Http/routes.php');
         
         $this->loadViewsFrom(__DIR__ . '/views', 'mikewazovzky-demo');
+        
+        $this->publishes([
+            __DIR__ . '/config/main.php' => config_path('mikewazovzky-demo.php'),
+            __DIR__ . '/views' => base_path('resources/views/vendor/mikewazovzky-demo'),
+        ]);
 	}
 	
 	public function register()
     {		
-		$this->app->bind('mikewazovzky-demo', function() {
+		$this->mergeConfigFrom(
+            __DIR__.'/config/main.php', 'mikewazovzky-demo'
+        );
+        
+        
+        $this->app->bind('mikewazovzky-demo', function() {
             return new Demo;
         });
+        
+        
     }	
 }
--- src/Http/DemoController.php
+++ src/Http/DemoController.php
@@ -5,8 +5,9 @@ use Illuminate\Routing\Controller as BaseController;
 
 class DemoController extends BaseController
 {
-    public function index($name)
+    public function index($name = null)
     {
+        $name = $name ?: config('mikewazovzky-demo.name');
         return view('mikewazovzky-demo::index', compact('name'));
     }
 }
